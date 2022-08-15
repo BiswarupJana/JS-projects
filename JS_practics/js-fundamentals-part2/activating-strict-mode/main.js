cc3c6ded1a1700855194f3b82b00573e23a59df8
Biswarup Jana <biswaruprx21@gmail.com>
2022-08-15 09:51:17 +0530

    practics

---
+++ JS_practics/js-fundamentals-part2/activating-strict-mode/main.js
@@ -0,0 +1,10 @@
+'use strict';
+let hasDriversLisense = false;
+const passTest =true;
+
+if(passTest) hasDriverLisense =true;
+if(hasDriversLisense) console.log('I can drive:D');
+
+const interface = 'Audio';
+
+const private =534;
---
+++ JS_practics/js-fundamentals-part2/function/main.js
@@ -0,0 +1,18 @@
+
+
+function logger(){
+    console.log('My name is joy');
+
+}
+
+//calling / running / invoking function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
