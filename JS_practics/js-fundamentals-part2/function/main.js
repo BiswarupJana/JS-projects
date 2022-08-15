e886b4d1cc4e2362bf921312fa95acea4d982f6c
Biswarup Jana <biswaruprx21@gmail.com>
2022-08-15 11:19:01 +0530

    function

--- JS_practics/js-fundamentals-part2/function/main.js
+++ JS_practics/js-fundamentals-part2/function/main.js
@@ -1,4 +1,4 @@
-
+// 'use strict';
 
 function logger(){
     console.log('My name is joy');
@@ -15,4 +15,45 @@ function fruitProcessor(apples, oranges){
     return juice;
 }
 
-fruitProcessor(5, 0);
+const appleJuice=fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice= fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+// function calcAge1(birthYear){
+//     const age = 2022-birthYear;
+//     return age;
+// }
+
+//function declaration
+function calcAge1(birthYear){
+    return 2022-birthYear;
+}
+
+const age1=calcAge1(1991);
+
+//function expression
+const calcAge2=function (birthYear){
+    return 2022-birthYear;
+}
+
+const age2=calcAge2(1991);
+console.log(age1, age2);
+
+
+//arrow function
+const calcAge3=birthYear=>2022-birthYear;
+const age3=calcAge3(1991);
+console.log(age3);
+
+
+const yearsUntilRetirement = (birthYear, firstName)=>{
+    const age =2022 -birthYear;
+    const retirement = 65-age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'joy'));
+console.log(yearsUntilRetirement(1980, 'jonus'));
