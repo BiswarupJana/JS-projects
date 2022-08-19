56625e7b33d2bed0a0eeb06df0d03a1d5ca5c489
Biswarup Jana <biswaruprx21@gmail.com>
2022-08-20 00:34:38 +0530

    loops end

--- JS_practics/js-fundamentals-part2/loop with array/main.js
+++ JS_practics/js-fundamentals-part2/loop with array/main.js
@@ -4,6 +4,15 @@
 
 let rep =1;
 while(rep<=10){
-    console.log(`Lifting weights repetition ${rep}`);
+    // console.log(`Lifting weights repetition ${rep}`);
     rep++;
+}
+
+let dice=Math.trunc(Math.random()*6)+1;
+// console.log(dice);
+
+while(dice!==6){
+    console.log(`you rolled a ${dice}`);
+    dice=Math.trunc(Math.random()*6)+1;
+    if(dice===6)console.log('loop is about to end');
 }
